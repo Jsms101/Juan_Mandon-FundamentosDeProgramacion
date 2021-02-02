fcb6dd9596cfa516f22849437736ae80ec897f8f
Jsms101 <50560118+Jsms101@users.noreply.github.com>
2021-02-02 04:47:23 -0500

    Add files via upload

---
+++ Desafio 02 (sistemas numericos) Triangulos_Juan mandon/Desafio 02 (sistemas numericos) Triangulos_Juan mandon/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Desafio_02__sistemas_numericos__Triangulos_Juan_mandon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variables
+            double hyperXY, topRightRad, cRad, eRad, aRad, bRad, dRad, noventaRad, bGrad, dGrad, t, y, z, b, d, e, x = 0; //NoventaRad es 90° por defecto (en radianes)
+
+
+            noventaRad = Math.PI / 2;
+
+            //Introduccion en consola al programa.
+            Console.WriteLine("Este es un programa para encontrar datos desconocidos de dos triangulos mediante diversos datos dados, en este caso" +
+                "\n d, b & y. ");
+
+            Console.WriteLine("\nEl programa entregara el valor de x");
+
+
+
+            //Toma de datos del usuario
+
+            Console.WriteLine("\n\n Digite el valor conocido de d en grados por favor.");
+
+            dGrad = double.Parse(Console.ReadLine());
+            dRad = dGrad * Math.PI / 180;                                 //grados a radianes
+
+            Console.WriteLine("\n\n Digite el valor conocido de b en grados por favor.");
+
+            bGrad = double.Parse(Console.ReadLine());
+            bRad = bGrad * Math.PI / 180;
+
+            Console.WriteLine("Digite el valor conocido de y por favor.");
+
+            y = double.Parse(Console.ReadLine());
+
+            //Procesamiento de datos para el triangulo pequeño
+
+            
+
+
+
+
+
+            //procesamiento de datos para el triangulo grande
+
+            eRad = Math.PI - (bRad + dRad); //con esto calculamos el lado de e, que es 180 - la suma de los dos angulos restantes
+
+            topRightRad = aRad + bRad;    //grados de la parte superior derecha del triangulo grande
+            d = Math.PI - noventaRad - topRightRad;
+
+            hyperXY = z * Math.Sin(topRightRad) / Math.Sin(d); //HyperXY es la suma de x + y, o el lado inferior del triangulo.
+
+            x = hyperXY - y;
+
+
+
+
+
+
+
+
+            //de radianes a grados
+
+            cRad = cRad * 180 / Math.PI;
+            aRad = aRad * 180 / Math.PI;
+
+
+            //Resultados
+
+            Console.WriteLine("Para los valores b = " + bGrad + " z = " + z + " & y = " + y + " Los valores desconocidos serian: \n ");
+            Console.WriteLine("x = " + x);
+            //Console.WriteLine("\nc = " + cRad);
+            //Console.WriteLine("\na = " + aRad);
+
+            Console.WriteLine("¡Gracias por usar la calculadora! Presione enter para terminar la aplicacion.");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
