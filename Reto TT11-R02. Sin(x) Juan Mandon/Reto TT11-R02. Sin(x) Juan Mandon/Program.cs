e8cdbe34e61813e58f9667387eeacfa87f57f3b5
Jsms101 <50560118+Jsms101@users.noreply.github.com>
2021-02-23 01:16:13 -0500

    Add files via upload

---
+++ Reto TT11-R02. Sin(x) Juan Mandon/Reto TT11-R02. Sin(x) Juan Mandon/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Reto_TT11_R02._Sin_x__Juan_Mandon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, i; // contador
+            double sinx, sinxii; 
+            x = i = sinx = sinxii = 0; //dos sinx para hacer los calculos de 
+                                       // "mayor nivel" (digeribles).
+
+
+            static double Factorial(double datoUser)  //factorizador 
+            {
+                double result = 1;          //5! = 5x4x3x2x1
+                                            // Factorial(8) 
+                //i is counter
+                for (double i = 1; i <= datoUser; i++)
+                {
+                    result *= i;
+                }
+
+                return result;
+            }
+
+            Console.WriteLine("Programa calculador de Sin(x)");
+            Console.WriteLine("Digite el valor de x por favor");
+
+            x = double.Parse(Console.ReadLine());
+
+            for (i = 0; i < 100; i++) 
+            {
+                sinx = (Math.Pow(-1, i))/(Factorial((2*i)+1));
+
+                sinxii += sinx * (Math.Pow(x, (2*i) + 1));
+
+                Console.WriteLine(sinxii);
+
+
+            }
+
+            if (i >= 100)
+            {
+                
+                Console.WriteLine("El valor de sin(" + x + ") es : " + sinxii + "");
+            }
+
+        }                  
+    }
+}
